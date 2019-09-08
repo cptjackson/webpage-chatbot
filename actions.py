cfdd85af3e19274820abd1c97ffbd10d93f50b19
Carl Jackson <carl.jackson@gmail.com>
2019-09-08 14:59:12 -0400

    Add experience form

--- actions.py
+++ actions.py
@@ -7,10 +7,43 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List, Union
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
+from rasa_sdk.events import SlotSet
+
+class ExperienceForm(FormAction):
+
+    """Gather information about what type of experience we want to talk about"""
+    def name(self):
+        """Unique identifier of the form"""
+        return "experience_form"
+
+    @staticmethod
+    def required_slots(tracker: Tracker) -> List[Text]:
+        """A list of required slots that the form has to fill"""
+
+        return ["work_type"]
+
+    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+        """A dictionary to map required slots to
+            - an extracted entity
+            - intent: value pairs
+            - a whole message
+            or a list of them, where a first match will be picked"""
+
+        return {
+            "work_type": self.from_entity(entity="work_type")
+        }
+
+    def submit(self, dispatcher, tracker, domain):
+        #dispatcher.utter_template("utter_submit_experience_form", tracker)
+        dispatcher.utter_message("Meep morp {}".format(tracker.get_slot("work_type")))
+        return []
+
+
 #
 #
 # class ActionHelloWorld(Action):
